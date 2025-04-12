03d4506c76e348be0cd0216b5dcada70c7f9bbfd
joseph32r <josephrounsville@gmail.com>
2025-04-12 15:30:58 -0600

    Create flags.js

---
+++ flags.js
@@ -0,0 +1,9 @@
+const flags = {
+  0: 'flag{AlamoBowlChamps}',
+  1: 'flag{inspect_element_master}',
+  2: 'flag{solaris}',
+  3: 'flag{dns_flag}',
+  4: 'flag{statueofliberty}',
+  5: 'flag{onepiece}',
+  final: 'itc101{best_class_ever}'
+};
